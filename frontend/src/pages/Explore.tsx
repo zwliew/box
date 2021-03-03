cfccabeca30aa96a24f09f114a5529fa9b6b5205
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-03-03 22:26:35 +0800

    Initial drawer

--- frontend/src/pages/Explore.tsx
+++ frontend/src/pages/Explore.tsx
@@ -7,8 +7,16 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
+  useDisclosure,
+  DrawerOverlay,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  Drawer,
+  Text,
 } from "@chakra-ui/react";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { useQuery } from "react-query";
 import { useLocation, Link as RouterLink } from "react-router-dom";
 import FileIcon from "../components/icons/FileIcon";
@@ -65,10 +73,12 @@ function FolderView({
   folders = [],
   files = [],
   root,
+  handleFileClick,
 }: {
   folders: string[];
   files: string[];
   root: string;
+  handleFileClick: (path: string) => void;
 }) {
   return (
     <>
@@ -81,7 +91,7 @@ function FolderView({
       </List>
       <List>
         {files.map((file: string) => (
-          <Link as={RouterLink} key={file} to={`${root}${file}`}>
+          <Link key={file} onClick={() => handleFileClick(`${root}${file}`)}>
             <FolderViewItem icon={FileIcon}>{file}</FolderViewItem>
           </Link>
         ))}
@@ -91,35 +101,109 @@ function FolderView({
 }
 
 function Explore() {
+  const [file, setFile] = useState<string | undefined>();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { pathname } = useLocation();
   const path = normalizePath(pathname);
-
   const url = `${API_URL}${path}`;
-  const { isLoading, error, data } = useQuery(`explore:${path}`, async () => {
+
+  const { isLoading, error, data } = useQuery(path, async () => {
     const res = await fetch(url);
     return res.json();
   });
+  const {
+    isLoading: isFileLoading,
+    error: fileError,
+    data: fileData,
+  } = useQuery(
+    ["file", file],
+    async () => {
+      const res = await fetch(`${API_URL}${file}`);
+      return res.json();
+    },
+    {
+      enabled: !!file,
+    }
+  );
 
   const pathFragments = decomposePathIntoFragments(path);
 
+  function handleFileClick(path: string) {
+    setFile(path);
+    onOpen();
+  }
+
+  function FileDetailsDrawer({
+    path,
+    data,
+  }: {
+    path?: string;
+    data: Record<string, any>;
+  }) {
+    if (isFileLoading) {
+      return <>Loading...</>;
+    }
+
+    if (fileError) {
+      return <Error error={error} />;
+    }
+
+    if (!path) {
+      return null;
+    }
+
+    return (
+      <>
+        <DrawerHeader>{path.slice(path.lastIndexOf("/") + 1)}</DrawerHeader>
+        <DrawerBody>
+          <List>
+            {Object.entries(data).map(([k, v]) => (
+              <ListItem key={k}>
+                <Text as="b">{k}</Text>: {v}
+              </ListItem>
+            ))}
+          </List>
+        </DrawerBody>
+      </>
+    );
+  }
+
   return (
-    <Box p={4}>
-      <Breadcrumb>
-        {pathFragments.map(({ display, actual }, index) => (
-          <BreadcrumbItem isCurrentPage={index === pathFragments.length - 1}>
-            <BreadcrumbLink as={RouterLink} to={actual}>
-              {display}
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-        ))}
-      </Breadcrumb>
-      <Error error={error} />
-      {isLoading ? (
-        <>Loading...</>
-      ) : (
-        <FolderView root={path} files={data.files} folders={data.folders} />
-      )}
-    </Box>
+    <>
+      <Box p={4}>
+        <Breadcrumb>
+          {pathFragments.map(({ display, actual }, index) => (
+            <BreadcrumbItem
+              key={actual}
+              isCurrentPage={index === pathFragments.length - 1}
+            >
+              <BreadcrumbLink as={RouterLink} to={actual}>
+                {display}
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          ))}
+        </Breadcrumb>
+        <Error error={error} />
+        {isLoading ? (
+          <>Loading...</>
+        ) : (
+          <FolderView
+            handleFileClick={handleFileClick}
+            root={path}
+            files={data.files}
+            folders={data.folders}
+          />
+        )}
+      </Box>
+      <Drawer isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerCloseButton />
+            <FileDetailsDrawer path={file} data={fileData} />
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </>
   );
 }
 
